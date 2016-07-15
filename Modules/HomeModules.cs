409568ae5b5aa2d39d583a3cf17b567d1dc46884
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-07-15 10:22:35 -0700

    add routes and views for create new and view all contacts

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -10,7 +10,17 @@ namespace AddressBook
     {
       Get["/"] = _ => {
         return View["index.cshtml"];
-      }
+      };
+
+      Get["/contact_view_all"] = _ => {
+
+        return View["contact_view_all.cshtml"];
+      };
+
+      Get["/contact_create"] = _ => {
+
+        return View["contact_create.cshtml"]
+      };
     }
   }
 }
